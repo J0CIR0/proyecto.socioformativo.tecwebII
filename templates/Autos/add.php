9ca6ccd0fe2280db35daa1752001f96b78876b34
Josue <clarosrocajosue@gmail.com>
2026-04-05 23:51:03 +0200

    feat: vistas para listar, crear y editar autos

---
+++ templates/Autos/add.php
@@ -0,0 +1,12 @@
+<h1><?= __('Add Car') ?></h1>
+<?= $this->Form->create($auto) ?>
+<?= $this->Form->control('marca', ['label' => __('Brand')]) ?>
+<?= $this->Form->control('modelo', ['label' => __('Model')]) ?>
+<?= $this->Form->control('tipo_combustible', ['label' => __('Fuel Type'), 'options' => $tiposCombustible]) ?>
+<?= $this->Form->control('estado', ['label' => __('Status'), 'options' => $estados]) ?>
+<?= $this->Form->control('fecha_limite', ['label' => __('Due Date'), 'type' => 'date']) ?>
+<?= $this->Form->control('descripcion_es', ['label' => 'Descripción (Español)', 'type' => 'textarea']) ?>
+<?= $this->Form->control('descripcion_en', ['label' => 'Description (English)', 'type' => 'textarea']) ?>
+<?= $this->Form->button(__('Save Car')) ?>
+<?= $this->Html->link('Cancelar', ['action' => 'index']) ?>
+<?= $this->Form->end() ?>
